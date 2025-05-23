7c8e7157a170c3a4757588c303e3fd724813bc41
seven <saintsaintro@gmail.com>
2025-05-23 19:35:10 +0500

    ⚡️ example

--- example/tamed_json_example.dart
+++ example/tamed_json_example.dart
@@ -1,23 +1,61 @@
 import 'dart:convert';
-import 'model_json_factory.dart';
-import 'product_model.dart';
+import 'package:tamed_json/tamed_json.dart';
 
-dynamic decode(Object? data) {
-  return jsonDecode(jsonEncode(data));
+/// lib/feature/product/data/model/product_model.dart
+class ProductModel {
+  const ProductModel({
+    required this.price,
+    required this.title,
+  });
+
+  final double price;
+  final String title;
+
+  @override
+  String toString() => 'ProductModel(price: $price, title: $title)';
+}
+
+/// lib/feature/product/boundary/product_model_json_factory.dart
+class ProductModelJsonFactory implements JsonFactory<ProductModel> {
+  const ProductModelJsonFactory();
+
+  @override
+  ProductModel fromJson(JsonMap json) {
+    return ProductModel(
+      price: json.double('price'),
+      title: json.string('title'),
+    );
+  }
+
+  @override
+  JsonMap toJson(ProductModel model) {
+    return {
+      'price': model.price,
+      'title': model.title,
+    };
+  }
 }
 
 void main() {
-  final startModels = [
-    ProductModel(price: 12, title: 'Phone'),
-    ProductModel(price: 2.1, title: 'Table'),
-  ];
+  final data = '''{
+      "phones":[{"price":12,"title":"Phone1"},{"price":2.1,"title":"Phone2"}, {"invalid_key": "invalid_value"}],
+      "computers":[{"price":20,"title":"PC1"},{"price":3.1,"title":"PC2"}]
+      }''';
+  final JsonMap json = jsonDecode(data);
 
-  const modelFactory = ProductModelJsonFactory();
-  final List jsonModel = decode(
-    startModels.map(modelFactory.toJson).toList(),
+  /// OPTION 1
+  final List<ProductModel> phones = json.factoryList(
+    const ProductModelJsonFactory(),
+    key: 'phones',
+    throws: false, // default: false. if true, throws exception on invalid data
   );
 
-  print(jsonModel);
+  /// OPTION 2
+  final List<ProductModel> pcs = json
+      .list('computers')
+      .map((e) => const ProductModelJsonFactory().fromJson(e))
+      .toList();
 
-  //TODO
+  print('phones:\t$phones');
+  print('pcs:\t$pcs');
 }
